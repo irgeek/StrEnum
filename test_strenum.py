d798d693a22a2b5150a2c4fed87ec24efbfc0a09
Chris Barnes <barnesc@janelia.hhmi.org>
2019-06-25 14:37:32 -0400

    Broaden tests
    Test for some other string-like behaviour, like comparison and hashing.
    
    Also test that StrEnums cannot be created with non-string values.

--- test_strenum.py
+++ test_strenum.py
@@ -1,10 +1,13 @@
 from enum import auto
+
+import pytest
+
 from strenum import StrEnum
 
 
 class HttpMethod(StrEnum):
     GET = auto()
-    HEAD = auto
+    HEAD = auto()
     POST = auto()
     PUT = auto()
     DELETE = auto()
@@ -24,3 +27,24 @@ def test_value_isinstance_str():
 
 def test_str_builtin():
     assert str(HttpMethod.GET) == "GET"
+
+
+def test_str_equals():
+    assert HttpMethod.GET == "GET"
+
+
+def test_str_hash():
+    assert hash(HttpMethod.GET) == hash("GET")
+
+
+def test_str_cmp():
+    assert HttpMethod.GET > "FET"
+    assert HttpMethod.GET < "GETA"
+
+
+def test_nonstring_fails():
+    with pytest.raises(TypeError):
+
+        class BadEnum(StrEnum):
+            ONE = 1
+            TWO = 2
