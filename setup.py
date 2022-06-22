60e0b332488b1886ff3774ff3dfbda6ba0a57aca
James Sinclair <james@nurfherder.com>
2022-06-22 07:59:46 +0200

    Stop installing "tests" dir as a package.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/irgeek/StrEnum",
-    packages=setuptools.find_packages(),
+    packages=["strenum"],
     extras_require={
         "test": [
             "pytest",
